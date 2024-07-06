5a3ad501776c090f9d6a275d9d4b6e4e226a33d1
Juan Carlos <gurquiza8@gmail.com>
2024-07-06 04:50:30 -0600

    Show mapBox

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoianVhbnVycXVpemEiLCJhIjoiY2x3Y2puYzQ4MHl6azJpbml2emNlcXJ0cyJ9.LJHP1EFX6xB4eM4lZLBEIQ';
 
 if ( !navigator.geolocation ) {
   alert('Navegador no soporta la Geolocalización');
