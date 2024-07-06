94ffd0bf8f2f7421d5937c58642995c11cb30927
Juan Carlos <gurquiza8@gmail.com>
2024-07-06 04:30:16 -0600

    First commit

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+if ( !navigator.geolocation ) {
+  alert('Navegador no soporta la Geolocalización');
+  throw new Error('Navegador no soporta la Geolocalización');
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
