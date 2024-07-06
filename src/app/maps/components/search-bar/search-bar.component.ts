50c0f50445bd191d685b0955940844bc232b1083
Juan Carlos <gurquiza8@gmail.com>
2024-07-06 12:12:32 -0600

    Request to mapbox for search places

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,14 +10,16 @@ export class SearchBarComponent {
 
   private debounceTimer?: NodeJS.Timeout;
 
-  constructor() {}
+  constructor(
+    private placesService: PlacesService,
+  ) {}
 
   onQueryChanged( query: string = '' ) {
 
     if ( this.debounceTimer ) clearTimeout( this.debounceTimer );
 
     this.debounceTimer = setTimeout(() => {
-      console.log(query);
+      this.placesService.getPlacesByQuery( query );
     }, 350);
 
   }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +9,16 @@ export class PlacesService {
 
   public useLocation?: [number, number];
 
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
+
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor() {
+  constructor(
+    private http: HttpClient
+  ) {
     this.getUserLocation();
   }
 
@@ -32,6 +39,22 @@ export class PlacesService {
       )
 
     });
+
+  }
+
+
+  getPlacesByQuery( query: string = '' ) {
+    // TODO: Evaluar cuando el query es nulo
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/search/geocode/v6/forward?q=${query}%20park&proximity=-99.13677199054422%2C19.442285518839626&access_token=pk.eyJ1IjoianVhbnVycXVpemEiLCJhIjoiY2x3Y2puYzQ4MHl6azJpbml2emNlcXJ0cyJ9.LJHP1EFX6xB4eM4lZLBEIQ`)
+      .subscribe( resp => {
+        console.log(resp.features);
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      });
+
 
   }
 
